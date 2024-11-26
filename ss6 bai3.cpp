9f5f6765c1fdda8b69b651857562407c185339c5
chuninh <chuninh2006@gmail.com>
2024-11-27 00:50:25 +0700

    Add files via upload

---
+++ ss6 bai3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int number;
+	printf("moi ban nhap mat khau\n");
+	scanf("%d",&number);
+	while(number!=2006){
+		
+		printf("nhap lai mat khau\n");
+		scanf("%d",&number);
+	}
+
+
+	return 0;
+	
+}
+	
+	
